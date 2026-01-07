0d9cbb32d017276e73a392b885a83d5dac99c78e
Daniel Piliutsin <102699155+DanielPiliutsin@users.noreply.github.com>
2026-01-07 02:18:46 -0700

    updated dynamic hue

--- .config/hypr/scripts/Switch_Wallpaper.sh
+++ .config/hypr/scripts/Switch_Wallpaper.sh
@@ -6,3 +6,4 @@ swww img $image_path --transition-type wipe --transition-fps 120
 wal -i $image_path -n ; hyprctl reload ; pkill -f "Kitty_Cava"
 kitty +kitten panel --edge=none --columns=2660px --lines=1500px --config "$HOME/.config/hypr/kittyconfigbg.conf" --margin-left=0 --margin-bottom=50 --name "Kitty_Cava" cava
 bash $HOME/.cache/wal/tclock.sh
+gsettings set org.gnome.desktop.interface gtk-theme FlatColor && gsettings set org.gnome.desktop.interface gtk-theme pywall-dynamic
